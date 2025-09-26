891605b9c4ae5b1f440af80fa6604fe29909cf1a
Zamman Asif <zamman2023@gmail.com>
2025-09-26 15:44:58 +0500

    robot.txt added and button dialog shadcn check

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,31 @@
 import { Code, Smartphone, Palette } from "lucide-react";
 import Footer from "@/Components/Footer";
-import Navbar from "@/Components/Navbar"
+import Navbar from "@/Components/Navbar";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
+import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
       <Navbar />
+<Dialog>
+  <DialogTrigger>Open</DialogTrigger>
+  <DialogContent>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+      <DialogDescription>
+        This action cannot be undone. This will permanently delete your account
+        and remove your data from our servers.
+      </DialogDescription>
+    </DialogHeader>
+  </DialogContent>
+</Dialog>
       {/* Hero Section */}
    <section
   className="relative h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat"
